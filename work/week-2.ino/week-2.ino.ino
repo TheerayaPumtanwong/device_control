e3bb2a121b95a7f9419a491486b91e004fe33ee8
TheerayaPumtanwong <traya1448@gmail.com>
2025-07-09 13:00:08 +0700

    week-2

--- work/week-2.ino/week-2.ino.ino
+++ work/week-2.ino/week-2.ino.ino
@@ -12,12 +12,12 @@ void loop() {
   
 
   digitalWrite(LED_BUILTIN, 1);
-  Serial.print("สวัสดี นันทพงศ์ อรุณเดช");
+  Serial.print("สวัสดี ธีรยา พุ่มตันวงษ์");
   Serial.print("light ...\n");
   delay(1000);
 
   digitalWrite(LED_BUILTIN, 0);
-  Serial.print("สวัสดี นันทพงศ์ อรุณเดช");
+  Serial.print("สวัสดี ธีรยา พุ่มตันวงษ์");
   Serial.print("not light ...\n");
   delay(1000);
 }
